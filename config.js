4f31d3e2966894408c6236f7f95b5dfd86b23d93
Andrew Lawson <andrewlawson234@gmail.com>
2020-11-09 11:31:33 -0800

    Workshop 4: Complete

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '647851402500014',
+        clientSecret: '9a80a50a830a0cd7da6c2d38ed5f9549'
+    }
 }
